f89c2d3d913e6f1502690c301dfd33065995c1e9
umbei <50173839+umbei@users.noreply.github.com>
2026-03-15 11:57:18 +0800

    Create diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,8 @@
+# diy-part1.sh 中添加
+# 添加 Passwall 依赖
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+# 添加 Lucky
+git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
+# 添加 SmartDNS
+git clone https://github.com/pymumu/luci-app-smartdns.git package/smartdns
